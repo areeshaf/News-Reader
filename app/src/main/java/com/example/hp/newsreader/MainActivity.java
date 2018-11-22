da521ef85b49fa0df17e035b19d5f2cc6ab4d99a
areeshaFatima <areeshaf7@gmail.com>
2018-11-23 01:58:08 +0530

    data in database

--- app/src/main/java/com/example/hp/newsreader/MainActivity.java
+++ app/src/main/java/com/example/hp/newsreader/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hp.newsreader;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sqLiteDatabase=this.openOrCreateDatabase("Articles",MODE_PRIVATE,null);
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS articles(id INTEGER PRIMARY KEY,articleid INTEGER,title VARCHAR,content VARCHAR) ");
+
+
         DownloadTask task=new DownloadTask();
         try{
             task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
@@ -40,9 +46,27 @@ public class MainActivity extends AppCompatActivity {
         listView=findViewById(R.id.listView);
         arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,titles);
         listView.setAdapter(arrayAdapter);
+        updateListView();
 
     }
 
+    public void updateListView(){
+        Cursor cursor=sqLiteDatabase.rawQuery("SELECT * FROM articles",null);
+        int titleIndex=cursor.getColumnIndex("title");
+        int contentIndex=cursor.getColumnIndex("content");
+
+        if(cursor.moveToFirst()) {
+            titles.clear();
+            content.clear();
+
+            do {
+                titles.add(cursor.getString(titleIndex));
+                content.add(cursor.getString(contentIndex));
+            } while (cursor.moveToNext());
+
+            arrayAdapter.notifyDataSetChanged();
+        }
+    }
     public class DownloadTask extends AsyncTask<String, Void, String>{
 
         @Override
@@ -65,10 +89,11 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 JSONArray jsonArray=new JSONArray(result);
-                int numberOfNews=10;
-                if(jsonArray.length()<10){
+                int numberOfNews=2;
+                if(jsonArray.length()<2){
                     numberOfNews=jsonArray.length();
                 }
+                sqLiteDatabase.execSQL("DELETE FROM articles");
                 for(int i=0;i<numberOfNews;i++){
                     String articleId=jsonArray.getString(i);
                    try{
@@ -85,11 +110,33 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         JSONObject jsonObject=new JSONObject(article);
-                        String articleTitle=jsonObject.getString("title");
-                        String articleURL= jsonObject.getString("url");
+                        if(!jsonObject.isNull("title")&& !jsonObject.isNull("url")) {
+                            String articleTitle = jsonObject.getString("title");
+                            String articleURL = jsonObject.getString("url");
 
-                        Log.i("Title and URL",articleTitle+"#######"+articleURL);
+                            url = new URL(articleURL);
+                            urlConnection = (HttpURLConnection) url.openConnection();
+                            inputStream = urlConnection.getInputStream();
+                            inputStreamReader = new InputStreamReader(inputStream);
+                            data = inputStreamReader.read();
+                            String htmlURLcontent = "";
+                            while (data != -1) {
+                                char current = (char) data;
+                                htmlURLcontent += current;
+                                data = inputStreamReader.read();
+                            }
+                            Log.i("HTML_URL", htmlURLcontent);
 
+                            String sql="INSERT INTO articles(articleId,title,content) VALUES (?,?,?)";
+                            SQLiteStatement sqLiteStatement=sqLiteDatabase.compileStatement(sql);
+                            sqLiteStatement.bindString(1,articleId);
+                            sqLiteStatement.bindString(2,articleTitle);
+                            sqLiteStatement.bindString(3,articleURL);
+                            sqLiteStatement.execute();
+
+
+                        }
+                        // Log.i("Title and URL",articleTitle+"#######"+articleURL);
                        // Log.i("ARTICLE",article);
 
                     }catch (Exception e){
@@ -104,5 +151,12 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            updateListView();
+        }
     }
+
 }
